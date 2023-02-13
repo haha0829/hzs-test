b1240713d2f5e72c1a68ce769430a7ef27cab4fe
huangzhangshu <huangzhangshu@infracreate.com>
2023-02-14 00:41:52 +0800

    chore: add Update Release ci

---
+++ .github/utils/utils.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+show_help() {
+cat << EOF
+Usage: $(basename "$0") <options>
+
+    -h, --help                Display help
+    -t, --type                Operation type
+                                1) remove v prefix
+                                2) replace '-' with '.'
+                                3) get release asset upload url
+                                4) update release latest
+    -tn, --tag-name           Release tag name
+    -gr, --github-repo        Github Repo
+    -gt, --github-token       Github token
+EOF
+}
+
+GITHUB_API="https://api.github.com"
+KUBEBLOCKS_REPO=JashBook/hzs-test
+
+main() {
+    local TYPE
+    local TAG_NAME
+    local GITHUB_REPO
+    local GITHUB_TOKEN
+
+    parse_command_line "$@"
+
+    if [[ $TYPE == 1 ]]; then
+        echo "${TAG_NAME/v/}"
+    elif [[ $TYPE == 2 ]]; then
+        echo "${TAG_NAME/-/.}"
+    elif [[ $TYPE == 3 ]]; then
+        get_upload_url
+    elif [[ $TYPE == 4 ]]; then
+        update_release_latest
+    fi
+
+}
+
+parse_command_line() {
+    while :; do
+        case "${1:-}" in
+            -h|--help)
+                show_help
+                exit
+                ;;
+            -t|--type)
+                if [[ -n "${2:-}" ]]; then
+                    TYPE="$2"
+                    shift
+                fi
+                ;;
+            -tn|--tag-name)
+                if [[ -n "${2:-}" ]]; then
+                    TAG_NAME="$2"
+                    shift
+                fi
+                ;;
+            -gr|--github-repo)
+                if [[ -n "${2:-}" ]]; then
+                    GITHUB_REPO="$2"
+                    shift
+                fi
+                ;;
+            -gt|--github-token)
+                if [[ -n "${2:-}" ]]; then
+                    GITHUB_TOKEN="$2"
+                    shift
+                fi
+                ;;
+            *)
+                break
+                ;;
+        esac
+
+        shift
+    done
+}
+
+gh_curl() {
+    curl -H "Authorization: token $GITHUB_TOKEN" \
+      -H "Accept: application/vnd.github.v3.raw" \
+      $@
+}
+
+get_upload_url() {
+    gh_curl -s $GITHUB_API/repos/$GITHUB_REPO/releases/tags/$TAG_NAME > release_body.json
+    echo $(jq '.upload_url' release_body.json) | sed 's/\"//g'
+}
+
+update_release_latest() {
+    latest_release_tag=`gh_curl -s $GITHUB_API/repos/$KUBEBLOCKS_REPO/releases/latest | jq -r '.tag_name'`
+
+    release_id=`gh_curl -s $GITHUB_API/repos/$GITHUB_REPO/releases/tags/$latest_release_tag | jq -r '.id'`
+
+    gh_curl -X PATCH \
+        $GITHUB_API/repos/$GITHUB_REPO/releases/$release_id \
+        -d '{"draft":false,"prerelease":false,"make_latest":true}'
+}
+
+main "$@"
